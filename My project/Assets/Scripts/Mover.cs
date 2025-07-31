fae4648f19dad7c16a6e4c97157a08937259b864
mansukh01 <mansukhsingh01@gmail.com>
2025-07-31 16:32:55 +0530

    Refactor Mover script and remove tutorial assets
    Replaced axis value fields in Mover.cs with a moveSpeed variable and dynamic input-based movement. Deleted all TutorialInfo and Readme assets and scripts. Updated main.unity scene to remove unused fields and adjust camera position. Changed active input handler in ProjectSettings.

--- My project/Assets/Scripts/Mover.cs
+++ My project/Assets/Scripts/Mover.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0.01f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0.01f;
-
+    [SerializeField] float moveSpeed = 10f;
        void Start()
     {
-         
+        
     }
-
         void Update()
-    {
+    {    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+         float yValue = 0.00f;
+         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+
         transform.Translate(xValue, yValue, zValue);
     }
 }
