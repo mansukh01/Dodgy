adcf1d791cd86bbf2568f0afbd8679b5eb0ed419
mansukh01 <mansukhsingh01@gmail.com>
2025-08-22 11:12:44 +0530

    movement code

--- My project/Assets/Scripts/Mover.cs
+++ My project/Assets/Scripts/Mover.cs
@@ -3,16 +3,27 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
-       void Start()
+    void Start()
     {
-        
+        PrintINstruction();
     }
-        void Update()
-    {    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-         float yValue = 0.00f;
-         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+    void Update()
 
+    {
+       MovePlayer();
+    }
 
+    void PrintINstruction()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Use WASD or Arrow keys to move the player");
+    }
+
+    void MovePlayer()
+    {
+         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0.00f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
 }
